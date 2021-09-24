aa5a3314681de6dd0cba6842e8e5abdbb9773dc5
Chibirev Grigoriy <2000grisha@gmail.com>
2021-09-25 02:36:48 +0300

    Add stage interface

---
+++ TranslatorLib/Stage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslatorLib
+{
+    public interface IStage<out E> where E : IStageElement
+    {
+        E TakeElement();
+
+        E CurrentElement { get; }
+    }
+
+    public interface IStageElement
+    {
+        bool IsLast();
+    }
+}
