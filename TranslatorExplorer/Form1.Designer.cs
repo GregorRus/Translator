f0a085c2b6bf1633ed8c6382bf31a75259fd6589
Chibirev Grigoriy <2000grisha@gmail.com>
2021-12-02 02:19:00 +0300

    [Explorer] Add UI elements for hash tables

--- TranslatorExplorer/Form1.Designer.cs
+++ TranslatorExplorer/Form1.Designer.cs
@@ -66,10 +66,18 @@ namespace TranslatorExplorer
             this.StageStatusStrip = new System.Windows.Forms.StatusStrip();
             this.StageToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.Hash1Label = new System.Windows.Forms.Label();
+            this.Hash2Label = new System.Windows.Forms.Label();
+            this.Hash3Label = new System.Windows.Forms.Label();
+            this.Hash1RichTextBox = new System.Windows.Forms.RichTextBox();
+            this.Hash2RichTextBox = new System.Windows.Forms.RichTextBox();
+            this.Hash3RichTextBox = new System.Windows.Forms.RichTextBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.StageStatusStrip.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // SourceRichTextBox
@@ -80,7 +88,7 @@ namespace TranslatorExplorer
             this.SourceRichTextBox.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.SourceRichTextBox.Location = new System.Drawing.Point(3, 39);
             this.SourceRichTextBox.Name = "SourceRichTextBox";
-            this.SourceRichTextBox.Size = new System.Drawing.Size(391, 334);
+            this.SourceRichTextBox.Size = new System.Drawing.Size(391, 206);
             this.SourceRichTextBox.TabIndex = 0;
             this.SourceRichTextBox.Text = "";
             this.SourceRichTextBox.MouseHover += new System.EventHandler(this.SourceRichTextBox_MouseHover);
@@ -92,7 +100,7 @@ namespace TranslatorExplorer
             this.ResultRichTextBox.Location = new System.Drawing.Point(400, 39);
             this.ResultRichTextBox.Name = "ResultRichTextBox";
             this.ResultRichTextBox.ReadOnly = true;
-            this.ResultRichTextBox.Size = new System.Drawing.Size(391, 334);
+            this.ResultRichTextBox.Size = new System.Drawing.Size(391, 206);
             this.ResultRichTextBox.TabIndex = 1;
             this.ResultRichTextBox.Text = "";
             // 
@@ -144,7 +152,7 @@ namespace TranslatorExplorer
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(794, 420);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(794, 292);
             this.tableLayoutPanel1.TabIndex = 5;
             // 
             // flowLayoutPanel1
@@ -152,7 +160,7 @@ namespace TranslatorExplorer
             this.flowLayoutPanel1.Controls.Add(this.ProcessComboBox);
             this.flowLayoutPanel1.Controls.Add(this.ProcessButton);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(400, 379);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(400, 251);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(391, 38);
             this.flowLayoutPanel1.TabIndex = 5;
@@ -196,15 +204,92 @@ namespace TranslatorExplorer
             this.tableLayoutPanel2.ColumnCount = 1;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.StageStatusStrip, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.StageStatusStrip, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(800, 450);
             this.tableLayoutPanel2.TabIndex = 6;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel3.Controls.Add(this.Hash1Label, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.Hash2Label, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.Hash3Label, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.Hash1RichTextBox, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.Hash2RichTextBox, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.Hash3RichTextBox, 2, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 301);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(794, 121);
+            this.tableLayoutPanel3.TabIndex = 6;
+            // 
+            // Hash1Label
+            // 
+            this.Hash1Label.AutoSize = true;
+            this.Hash1Label.Location = new System.Drawing.Point(3, 0);
+            this.Hash1Label.Name = "Hash1Label";
+            this.Hash1Label.Size = new System.Drawing.Size(107, 20);
+            this.Hash1Label.TabIndex = 0;
+            this.Hash1Label.Text = "Первое слово";
+            // 
+            // Hash2Label
+            // 
+            this.Hash2Label.AutoSize = true;
+            this.Hash2Label.Location = new System.Drawing.Point(267, 0);
+            this.Hash2Label.Name = "Hash2Label";
+            this.Hash2Label.Size = new System.Drawing.Size(104, 20);
+            this.Hash2Label.TabIndex = 1;
+            this.Hash2Label.Text = "Второе слово";
+            // 
+            // Hash3Label
+            // 
+            this.Hash3Label.AutoSize = true;
+            this.Hash3Label.Location = new System.Drawing.Point(531, 0);
+            this.Hash3Label.Name = "Hash3Label";
+            this.Hash3Label.Size = new System.Drawing.Size(141, 20);
+            this.Hash3Label.TabIndex = 2;
+            this.Hash3Label.Text = "Знаки препинания";
+            // 
+            // Hash1RichTextBox
+            // 
+            this.Hash1RichTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Hash1RichTextBox.Location = new System.Drawing.Point(3, 39);
+            this.Hash1RichTextBox.Name = "Hash1RichTextBox";
+            this.Hash1RichTextBox.Size = new System.Drawing.Size(258, 79);
+            this.Hash1RichTextBox.TabIndex = 3;
+            this.Hash1RichTextBox.Text = "";
+            // 
+            // Hash2RichTextBox
+            // 
+            this.Hash2RichTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Hash2RichTextBox.Location = new System.Drawing.Point(267, 39);
+            this.Hash2RichTextBox.Name = "Hash2RichTextBox";
+            this.Hash2RichTextBox.Size = new System.Drawing.Size(258, 79);
+            this.Hash2RichTextBox.TabIndex = 4;
+            this.Hash2RichTextBox.Text = "";
+            // 
+            // Hash3RichTextBox
+            // 
+            this.Hash3RichTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Hash3RichTextBox.Location = new System.Drawing.Point(531, 39);
+            this.Hash3RichTextBox.Name = "Hash3RichTextBox";
+            this.Hash3RichTextBox.Size = new System.Drawing.Size(260, 79);
+            this.Hash3RichTextBox.TabIndex = 5;
+            this.Hash3RichTextBox.Text = "";
             // 
             // Form1
             // 
@@ -222,6 +307,8 @@ namespace TranslatorExplorer
             this.StageStatusStrip.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -240,6 +327,13 @@ namespace TranslatorExplorer
         private System.Windows.Forms.StatusStrip StageStatusStrip;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.ToolStripStatusLabel StageToolStripStatusLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label Hash1Label;
+        private System.Windows.Forms.Label Hash2Label;
+        private System.Windows.Forms.Label Hash3Label;
+        private System.Windows.Forms.RichTextBox Hash1RichTextBox;
+        private System.Windows.Forms.RichTextBox Hash2RichTextBox;
+        private System.Windows.Forms.RichTextBox Hash3RichTextBox;
     }
 }
 
